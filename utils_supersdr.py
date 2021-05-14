5d929fd3b93fc3ff2f56ad019ec3c117a71c7bf1
Marco Cogoni <cogoni@gmail.com>
2021-05-14 17:06:17 +0200

    little fix to kiwi sound parameter reading

--- utils_supersdr.py
+++ utils_supersdr.py
@@ -648,7 +648,7 @@ class kiwi_sound():
                 msg = self.stream.receive_message()
                 if msg and "SND" == bytearray2str(msg[:3]):
                     break
-                elif "MSG audio_init" in bytearray2str(msg):
+                elif msg and "MSG audio_init" in bytearray2str(msg):
                     msg = bytearray2str(msg)
                     els = msg[4:].split()                
                     self.KIWI_RATE = int(int(els[1].split("=")[1]))
