improved s-meter precision (over S9 it's still wrong); fixed spectrum dynamic tuning

supersdr.py
@@ -204,10 +204,10 @@ def s_meter_draw(rssi_smooth, agc_threshold):
 
     s_meter_center = (s_meter_radius+10,s_meter_radius+8)
     alpha_rssi = rssi_smooth+127
-    alpha_rssi = -math.radians(alpha_rssi* 180/127.)-math.pi
+    alpha_rssi = -math.radians(alpha_rssi * 180/127.)-math.pi*1.02
 
     alpha_agc = agc_threshold+127
-    alpha_agc = -math.radians(alpha_agc* 180/127.)-math.pi
+    alpha_agc = -math.radians(alpha_agc * 180/127.)-math.pi*1.02
 
     def _coords_from_angle(angle, s_meter_radius_):
         x_ = s_meter_radius_ * math.cos(angle)
@@ -223,7 +223,7 @@ def s_meter_draw(rssi_smooth, agc_threshold):
     pygame.draw.rect(smeter_surface, BLACK,
                    (s_meter_center[0]-60, s_meter_center[1]-58, SMETER_XSIZE, SMETER_YSIZE), 3)
     
-    angle_list = np.linspace(0.4, math.pi-0.4, 9)
+    angle_list = np.linspace(0.2, math.pi-0.2, 9)
     text_list = ["1", "3", "5", "7", "9", "+10", "+20", "+30", "+40"]
     for alpha_seg, msg in zip(angle_list, text_list[::-1]):
         text_x, text_y = _coords_from_angle(alpha_seg, s_meter_radius*0.8)
@@ -531,6 +531,8 @@ rssi_maxlen = 10 # buffer length used to smoothen the s-meter
 rssi_hist = deque(rssi_maxlen*[kiwi_snd.rssi], rssi_maxlen)
 rssi_smooth = kiwi_snd.rssi
 run_index = 0
+run_index_bigmsg = 0
+
 run_index_automode = 0
 show_bigmsg = None
 msg_text = ""
@@ -549,7 +551,7 @@ while not wf_quit:
     force_sync_flag = None
 
     rssi = kiwi_snd.rssi
-    rssi_hist.append(rssi+10)
+    rssi_hist.append(rssi)
     mouse = pygame.mouse.get_pos()
 
     for event in pygame.event.get():
@@ -707,13 +709,14 @@ while not wf_quit:
 
                 # KIWI RX volume UP/DOWN, Mute
                 if keys[pygame.K_v] and (mods & pygame.KMOD_SHIFT):
-                    if kiwi_snd.volume > 0:
-                        old_volume = kiwi_snd.volume
-                        kiwi_snd.volume = 0
-                    else:
-                        kiwi_snd.volume = old_volume
-                    show_bigmsg = "VOLUME"
-                    run_index_bigmsg = run_index
+                    if run_index_bigmsg < run_index:
+                        if kiwi_snd.volume > 0:
+                            old_volume = kiwi_snd.volume
+                            kiwi_snd.volume = 0
+                        else:
+                            kiwi_snd.volume = old_volume
+                        show_bigmsg = "VOLUME"
+                        run_index_bigmsg = run_index
                 elif keys[pygame.K_v]:
                     if kiwi_snd.volume < 150:
                         kiwi_snd.volume += 10

utils_supersdr.py
@@ -543,24 +543,20 @@ class kiwi_waterfall():
     def spectrum_db2col(self):
         wf = self.spectrum
         wf = -(255 - wf)  # dBm
-        wf_db = wf - 13 + (3*self.zoom) # typical Kiwi wf cal
+        wf_db = wf - 13 + (3*self.zoom) # typical Kiwi wf cal and zoom correction
         wf_db[0] = wf_db[1] # first bin is broken
         
-        min_db, max_db = np.min(wf_db), np.max(wf_db)
-        #wf_db = np.clip(wf_db, min_db+self.delta_low_db, max_db+self.delta_high_db)
-
         if self.wf_auto_scaling:
             # compute min/max db of the power distribution at selected percentiles
             self.low_clip_db = np.percentile(wf_db, self.CLIP_LOWP)
             self.high_clip_db = np.percentile(wf_db, self.CLIP_HIGHP)
-            self.dynamic_range = max(self.high_clip_db-(self.low_clip_db+self.delta_low_db), self.MIN_DYN_RANGE)
+            self.dynamic_range = max(self.high_clip_db - self.low_clip_db, self.MIN_DYN_RANGE)
 
         # shift chosen min to zero
         wf_color_db = (wf_db - (self.low_clip_db+self.delta_low_db))
         # standardize the distribution between 0 and 1 (at least MIN_DYN_RANGE dB will be allocated in the colormap if delta=0)
-        # normal_factor_db = max(self.high_clip_db-(self.low_clip_db+self.delta_low_db), self.MIN_DYN_RANGE)+self.delta_high_db
-        normal_factor_db = self.dynamic_range+self.delta_high_db
-        self.wf_color = wf_color_db / normal_factor_db
+        normal_factor_db = self.dynamic_range + self.delta_high_db
+        self.wf_color = wf_color_db / (normal_factor_db-self.delta_low_db)
 
         self.wf_color = np.clip(self.wf_color, 0.0, 1.0)
 
@@ -780,7 +776,7 @@ class kiwi_sound():
         #flags,seq, = struct.unpack('<BI', buffer(data[0:5]))
         if bytearray2str(data[0:3]) == "SND": # this is one waterfall line
             s_meter, = struct.unpack('>H',  buffer(data[8:10]))
-            self.rssi = 0.1 * s_meter - 127
+            self.rssi = (0.1 * s_meter - 127)
             data = data[10:]
             count = len(data) // 2
             samples = np.ndarray(count, dtype='>h', buffer=data).astype(np.int16)